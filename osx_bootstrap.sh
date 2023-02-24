ab864fcdf3a4ee71fd99797912a05b0256c1ad83
Nils Magne Lunde <nilsml@nilsland.com>
2023-02-24 10:05:17 +0100

    Add tmux stuff

--- osx_bootstrap.sh
+++ osx_bootstrap.sh
@@ -116,6 +116,9 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+echo "Installing tmux plugin manager"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 echo "Installing global npm packages..."
 sudo -u $SUDO_USER npm install pnpm -g
 
